07be4cc8410a9db51992939f589dd78c1551d860
Marshalyel <marshalyel@gmail.com>
2025-03-01 10:49:17 +0900

    Update prev.js

--- plugins/prev.js
+++ plugins/prev.js
@@ -34,12 +34,8 @@ module.exports = {
       { buttonId: `.ytmp4 ${videoUrl}`, buttonText: { displayText: `.ytmp4 ${videoUrl}` }, type: 1 }
     ];
     
-    if (index > 0) {
+    if (index >= 0) {
       buttons.push({ buttonId: `.prev`, buttonText: { displayText: '.prev' }, type: 1 });
-      buttons.push({ buttonId: `.next`, buttonText: { displayText: '.Next' }, type: 1 });
-    }
-    
-    if (index < videos.length - 1) {
       buttons.push({ buttonId: `.next`, buttonText: { displayText: '.Next' }, type: 1 });
     }
     
